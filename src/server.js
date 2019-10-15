101f71d294e5a7f48b483e81d5699be4e2a7f433
claudiokorb <claudiokorb@hotmail.com>
2019-10-15 16:46:49 -0300

    Added database integration

--- src/server.js
+++ src/server.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const app = new express();
-let data;
+const admin = require('firebase-admin');
+var id_Sensor;
+var data_Sensor;
+var nivel_Sensor;
+
+
+var serviceAccount = require("../auth/admin.json");
+
+var fire_app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://color-classifier-7f089.firebaseio.com"
+});
+var db = admin.database();
+
+app.get('/view', function(request, response){
+  response.sendFile('/serv/src/table.html');
+});
+
 
 app.get('/update', function(request, response){
   response.sendFile('/serv/src/index.html');
-  response.send(request.query.dia + " " + request.query.chuva);
-  //response.send(request.query.chuva);
-  console.log("Dia: " + request.query.dia);
-  console.log("Chuva: " + request.query.chuva);
+  id_Sensor = request.query.id;
+  nivel_Sensor = request.query.chuva;
+  data_Sensor = request.query.dia;
+  var ref = db.ref("dados-chuva");
+  var agora = new Date();
+
+  ref.push(
+    {
+        Data: agora.getTime(),
+        Id: id_Sensor,
+        Nivel: nivel_Sensor
+    },
+        function(error){
+        if(error){
+          console.log("Data could not be saved: " +error);
+        }else{
+          console.log("Data saved successfully.");
+        }
+  });
+
+
 });
+
 app.listen(8180);
